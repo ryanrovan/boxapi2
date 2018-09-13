a842b66473df786b004c2eb7d8ffafb667f6deaf
ryanrovan <git@apimatic.io>
2018-09-13 20:23:00 +0000

    ryan

---
+++ BoxFoldersRTX.Standard/Configuration.cs
@@ -0,0 +1,11 @@
+using BoxFoldersRTX.Standard.Models;
+namespace BoxFoldersRTX.Standard
+{
+    public partial class Configuration
+    {
+
+        //The base Uri for API calls
+        public static string BaseUri = "https://api.box.com/2.0";
+
+    }
+}
